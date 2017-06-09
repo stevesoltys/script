a2b376af70ee4e333c25067b9948ef2aa5c6ec5d
Daniel Micay <danielmicay@gmail.com>
2017-06-08 21:37:16 -0400

    add metadata generation script

---
+++ generate_metadata.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from argparse import ArgumentParser
+from zipfile import ZipFile
+
+parser = ArgumentParser(description="Generate update server metadata")
+parser.add_argument("zip")
+
+with ZipFile(parser.parse_args().zip) as f:
+    with f.open("META-INF/com/android/metadata") as metadata:
+        data = dict(line[:-1].decode().split("=") for line in metadata)
+        with open(data["pre-device"] + "-testing", "w") as output:
+            build_id = data["post-build"].split("/")[3]
+            print(data["post-build-incremental"], data["post-timestamp"], build_id, file=output)
