df43f87c80073e3780de32bded5c8de252db1c87
Daniel Micay <danielmicay@gmail.com>
2017-06-29 01:43:22 -0400

    change delta generation order

--- generate_deltas.sh
+++ generate_deltas.sh
@@ -2,8 +2,8 @@
 
 [[ $# -eq 4 ]] || exit 1
 
-for old in $2 $3 $4; do
-  for device in angler bullhead marlin sailfish; do
+for device in angler bullhead marlin sailfish; do
+  for old in $2 $3 $4; do
     script/generate_delta.sh $device $old $1
   done
 done
