8bc5872ece1ef448db355e3533ddb037c573cbc1
Daniel Micay <danielmicay@gmail.com>
2017-08-06 20:00:37 -0400

    copperhead.sh: print $BUILD_NUMBER

--- copperhead.sh
+++ copperhead.sh
@@ -3,5 +3,6 @@ source build/envsetup.sh
 export LANG=C
 export _JAVA_OPTIONS=-XX:-UsePerfData
 export BUILD_NUMBER=$(date --utc +%Y.%m.%d.%H.%M.%S)
+echo "BUILD_NUMBER=$BUILD_NUMBER"
 export DISPLAY_BUILD_NUMBER=true
 chrt -b -p 0 $$
