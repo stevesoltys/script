9892a0a37209a100f8b9034bdaf2c0683a065d68
Daniel Micay <danielmicay@gmail.com>
2017-08-07 17:33:35 -0400

    update to android-7.1.2_r28 (N2G48C)

--- manage.sh
+++ manage.sh
@@ -9,8 +9,8 @@ fi
 
 kernel_suffix=nougat-mr2
 branch=nougat-mr2-release
-aosp_version=N2G48B
-aosp_tag=android-7.1.2_r19
+aosp_version=N2G48C
+aosp_tag=android-7.1.2_r28
 
 aosp_forks=(
   device_common
