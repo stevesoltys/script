716afc3d18b3628bba67fced62f3adf799b43a86
Daniel Micay <danielmicay@gmail.com>
2017-08-26 15:33:49 -0400

    remove minutes/seconds from generated BUILD_NUMBER

--- copperhead.sh
+++ copperhead.sh
@@ -2,7 +2,7 @@ source build/envsetup.sh
 
 export LANG=C
 export _JAVA_OPTIONS=-XX:-UsePerfData
-export BUILD_NUMBER=$(date --utc +%Y.%m.%d.%H.%M.%S)
+export BUILD_NUMBER=$(date --utc +%Y.%m.%d.%H)
 echo "BUILD_NUMBER=$BUILD_NUMBER"
 export DISPLAY_BUILD_NUMBER=true
 chrt -b -p 0 $$
