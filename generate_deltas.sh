54c97c3c66b4006c2037412b66a80149eee77b42
Daniel Micay <danielmicay@gmail.com>
2017-09-13 17:43:58 -0400

    drop angler / bullhead (Pixel only branch)

--- generate_deltas.sh
+++ generate_deltas.sh
@@ -2,7 +2,7 @@
 
 [[ $# -eq 4 ]] || exit 1
 
-for device in angler bullhead marlin sailfish; do
+for device in marlin sailfish; do
   for old in $2 $3 $4; do
     script/generate_delta.sh $device $old $1
   done
