8eb7cd9c0b898cdbee115976e5cda4a324bdb2a5
Daniel Micay <danielmicay@gmail.com>
2017-10-01 12:46:54 -0400

    drop platform_external_wpa_supplicant_8 repository

--- manage.sh
+++ manage.sh
@@ -35,7 +35,6 @@ aosp_forks=(
   platform_external_svox
   platform_external_sqlite
   platform_external_v8
-  platform_external_wpa_supplicant_8
   platform_frameworks_av
   platform_frameworks_base
   platform_frameworks_ex
